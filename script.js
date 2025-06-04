c59d3f14a9ddde453def6af1cfa5cee062ee228e
Theo Martin Prosise <theoprosise@meta.com>
2025-06-04 15:49:41 -0700

    implemented user review feature

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ document.addEventListener("DOMContentLoaded",() => {
     console.log("page loaded");
     loadReviews();
     let form = document.getElementById("review-form");
-    form.addEventListener('submit',() => {
-        handleReviewSubmit();
+    form.addEventListener('submit', handleReviewSubmit);
     });
-});
 
 
 function loadReviews() {
@@ -28,8 +26,28 @@ function loadReviews() {
 }
 
 
-function handleReviewSubmit(){
+const handleReviewSubmit = (event) =>{
     console.log("handleReviewSubmit");
+    event.preventDefault();
+
+    const reviewInput = document.querySelector("#review-text");
+    const reviewText = reviewInput.value;
+
+    const reviewRate = document.getElementById("rating");
+    const reviewRating = reviewRate.value;
+
+    const reviewTitle = document.getElementById("book-title");
+    const reviewTit = reviewTitle.value;
+    let review = {
+        reviewText,
+        title: reviewTit,
+        reviewRating
+    };
+    let revEl = createReviewElement(review);
+
+    const revPar = document.getElementById("reviews-list");
+    revPar.appendChild(revEl);
+
 }
 
 function createReviewElement(review){
